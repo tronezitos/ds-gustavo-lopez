e13f999297dbead68170b3da4c23cd3b5354b6bc
Gustavo Lopez <gugianelli@gmail.com>
2025-02-17 09:18:53 -0300

    Aula sobre estrutura e códigos básicos em java

---
+++ aula01/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Teu pai de calcinha");
+      System.out.println("Bom dia pobres");
+    }
+  }
---
+++ aula01/Numeros.java
@@ -0,0 +1,8 @@
+public class Numeros {
+     public static void main(String[] args) {
+        System.out.println(9 + 10);
+        System.out.println(10 - 5);
+        System.out.println(3 * 20);
+        System.out.println(14 / 2);
+    }
+}

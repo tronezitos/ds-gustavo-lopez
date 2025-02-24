1b05440e6f909af73efb8a99d9ef250d3c8f0744
Gustavo Lopez <gugianelli@gmail.com>
2025-02-24 09:31:21 -0300

    Aula de Java

---
+++ aula02/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args){
+        String msg = "Olá Mundo";
+        System.out.println(msg);
+        int n1 = 3;
+        int n2 = 4;
+        int n3 = 23;
+        System.out.println(n1);
+        System.out.println(n1 + n2);
+        System.out.println(n1 + n2 * n3);
+        String msg1 = "Flamengo FC";
+        System.out.println(msg1);
+
+
+
+    }
+}

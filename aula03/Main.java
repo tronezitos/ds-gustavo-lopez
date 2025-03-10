5414e87409d16d4ac283510d588d9cb359b6b8e4
Gustavo Lopez <gugianelli@gmail.com>
2025-03-10 09:18:37 -0300

    Aula 3 sobre Data Types e User Input

---
+++ aula03/Main.java
@@ -0,0 +1,27 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int myNum = 5; // integer (whole number)
+        float myFloatNum = 5.99f; // floating point number
+        char myLetter = 'D'; // character
+        boolean myBool = true; // boolean
+        String myText = "Hello"; // String
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+
+        float f1 = 233f;
+        System.out.println(f1);
+
+        double myInt = 12.43;
+        byte myDouble = (byte) myInt; // Automatic casting: int to double
+
+        System.out.println(myInt); // Outputs 9
+        System.out.println(myDouble); // Outputs 9.0
+
+    }
+}

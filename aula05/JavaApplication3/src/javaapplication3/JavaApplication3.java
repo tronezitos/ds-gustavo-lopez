fa8a69efcb9775aa57aa7bff617ddab6bdd40858
Gustavo Lopez <gugianelli@gmail.com>
2025-04-28 08:57:21 -0300

    Aula 5 de Java

---
+++ aula05/JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,18 @@
+package javaapplication3;
+import java.util.Scanner;
+
+public class JavaApplication3 {
+
+    public static void main(String[] args) {
+       
+        int gastosJaneiro = 15000;
+        int  gastosFevereiro = 23000;
+        int gastosMarco = 17000;
+        
+        int gastoTrimestral = gastosJaneiro + gastosFevereiro + gastosMarco;
+        
+        System.out.println("O gasto trimestral e de: " + gastoTrimestral);
+        
+   }
+    
+}
